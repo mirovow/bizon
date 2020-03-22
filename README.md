ceb7781db39fa5af847d2b75601025e04c8e11b3
Alex MirovoW <mirovow@gmail.com>
2020-03-22 17:46:42 +0500

    refactor(*): refactor all project

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Bizon
-Bizon is a simple library that allows you to run functions in a worker thread of Node.JS or Web Workers of a browser.
+Bizon is a simple library that allows you to run functions in a worker thread of Node.JS.
 
 ## Installation
 ```
@@ -9,67 +9,134 @@ npm i --save bizon
 ```js
 require('bizon');
 
-function fib(n) {
-  return n <= 1 ? n : fib(n - 1) + fib(n - 2);
-}
-
-// You need to call a function with .$ and it will be run in a worker thread of Node.JS or Web Worker in a browser
-// .$ returns a Promise
-fib.$(25).then(result => console.log('fib(25)', result));
-
-// Also you can run a second function, and all of them will work in parallel
-fib.$(28).then(result => console.log('fib(28)', result));
-
-// You can pass a function to the arguments
-function customMap(arr, handler) {
-  return arr.map(handler);
-}
-
-customMap.$([43, 34, 23], (n) => {
+const fibonacci = Function.thread((n) => {
+  // You can not use recursive call fibonacci(), because the function body of the thread
+  // function can not get outside context
   function fib(n) {
     return n <= 1 ? n : fib(n - 1) + fib(n - 2);
   }
-  return fib(n);
+ return fib(n);
+});
+
+// Then you can call the thread function which returns a Promise().
+fibonacci(25).then(result => console.log('fibonacci(25)', result));
+
+// Also you can run a second function, and all of them will work in parallel
+fibonacci(35).then(result => consolwe.log('fibonacci(28)', result));
+```
+### Using imports
+```js
+require('bizon');
+const fs = require('fs');
+
+const readFile = Function.thread((pathToFile) => {
+    // You can use modules which required in the parent thread
+    return fs.readFileSync(pathToFile).toString();
+});
+
+void async function () {
+    const fileData = await readFile('./README.md');
+    console.log(fileData);
+}();
+```
+### 'this' in a thread function
+You can use 'this' context
+```js
+require('bizon');
+void async function() {
+    const person = {
+      age: 34,
+      changeAge: Function.thread((newAge) => {
+        this.age = newAge;
+      }),
+    };
+    
+    console.log(person.age); // 34
+    await person.changeAge(50);
+    console.log(person.age); // 50
+}();
+```
+### Arguments
+You can pass numbers, strings, arrays, objects and functions to the arguments
+```js
+require('bizon');
+const customMap = Function.thread((arr, handler) => {
+  return arr.map(handler);
+});
+
+customMap([43, 34, 23], (n) => {
+  return String(n);
 }).then(result => console.log(result));
 
-setInterval(() => {
-  console.log("I'm not blocked!");
-}, 200);
+// The passed function have 'this' context of the parent function
+void async function() {
+    const obj = {
+        someConst: 3.14,
+        customMap: Function.thread((arr, callback) => {
+            return arr.map(callback);
+        }),
+    };
+    
+    const result = await obj.customMap([1, 2, 3], (val) => {
+        return this.someConst * val * val;
+    });
+    
+    console.log(result); // [ 3.14, 12.56, 28.259999999999998 ]
+}();
 ```
 ## Restrictions
+You can not use recursive calls
+```js
+require('bizon');
+const fib = Function.thread((n) => {
+  return n <= 1 ? n : fib(n - 1) + fib(n - 2);
+});
+
+fib().then(result => console.log(result));
+
+// UnhandledPromiseRejectionWarning: Error: fib is not defined
+```
 You can not get access to variables defined outside the function
 ```js
+require('bizon');
+
 const a = 10;
 
-function func() {
+const func = Function.thread(() => {
   return a + 5;
-}
-func.$().then(result => console.log(result));
+});
+
+func().then(result => console.log(result));
 
 // Error: ReferenceError: a is not defined
 ```
 You can not return objects and arrays with promises and functions
 ```js
-function func() {
+require('bizon');
+
+const func = Function.thread(() => {
     return {
       foo: new Promise((resolve) => resolve('hello')),
       baz: function() {
         return 'hello'
       }
     };
-}
-func.$().then(result => console.log(result));
+});
+
+func().then(result => console.log(result));
 
 //  Error: DataCloneError: #<Promise> could not be cloned.
 ```
 You can not return functions
 ```js
-function func() {
+require('bizon');
+
+const func = Function.thread(() => {
     return function() {
       return 'hello';
     }
-}
-func.$().then(result => console.log(result));
+});
+func().then(result => console.log(result));
 
 // Error: DataCloneError: function () {
 //   return 'hello';
--- src/index.ts
+++ src/index.ts
@@ -1,132 +1,189 @@
 interface Function {
-  $(...args): Promise<any>;
+  thread<A extends any[], R>(fn: (...args: A) => R, includeRequiredModules?: boolean): (...args: A) => Promise<R>;
 }
 
-void function() {
-  function checkGlobalVar(varName: string) {
-    return this.hasOwnProperty(varName);
+function stringifyFunctionBody(fn) {
+  const fnString = fn.toString();
+  return fnString.substring(fnString.indexOf("{") + 1, fnString.lastIndexOf("}"));
+}
+
+function stringifyFunctionArguments(fn) {
+  const fnString = fn.toString();
+  return fnString.substring(fnString.indexOf("(") + 1, fnString.indexOf(")"));
+}
+
+function getPathToParentModule(level: number = 3) {
+  const stack = new Error().stack.split('\n');
+  let trace;
+  if (stack.length >= 3) {
+    trace = `${stack[level].replace('\t', '').trim()}`;
+  } else {
+    trace = `${stack[stack.length - 1].replace('\t', '').trim()}`;
   }
+  const a = trace.split(':');
+  a.splice(-2);
+  const b = a.join(':');
+  return b.substring(b.indexOf('/')) || ``;
+}
 
-  function convertArgsToString(args) {
-    return args.map(arg => {
-      if (arg === undefined) {
-        return 'undefined';
-      }
-      if (arg === null) {
-        return 'null';
-      }
-      if (typeof arg === 'string') {
-        return `"${ arg }"`
-      }
-      if (typeof arg === 'object') {
-        return JSON.stringify(arg, (key, value) => {
-          if (value === undefined) {
-            return 'undefined';
-          }
-          return typeof value === 'function'
-            ? delete value.toString && value.toString()
-            : value;
-        }).replace(/"undefined"/g, 'undefined');
-      } else {
-        return delete arg.toString && arg.toString();
-      }
-    });
+async function getRequiredModulesString(modulePath: string, includeRequiredModules: boolean = true): Promise<string> {
+  if (includeRequiredModules) {
+    const fs = require('fs');
+    const callerModuleText: string = await new Promise<string>((resolve, reject) => fs.readFile(modulePath, (err, result) => {
+      if (err) reject(err);
+      else resolve(result.toString());
+    }));
+    
+    const requireMatches = callerModuleText.match(/["'\s]*([\w*{}\n\r\t, ]+)(\s+|\s)=\s+require\(\s*["'\s].*([@\w_-]+)["'\s].*\).$/gm);
+    if (requireMatches) {
+      return requireMatches.join(';');
+    }
   }
+  return '';
+}
 
-  const getTypescriptCompilerHelpers = () => `
-    var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {return new (P || (P = Promise))(function (resolve, reject) {function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }step((generator = generator.apply(thisArg, _arguments || [])).next());});};
-    var __generator = (this && this.__generator) || function (thisArg, body) {var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;function verb(n) { return function (v) { return step([n, v]); }; }function step(op) {if (f) throw new TypeError("Generator is already executing.");while (_) try {if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;if (y = 0, t) op = [op[0] & 2, t.value];switch (op[0]) {case 0: case 1: t = op; break;case 4: _.label++; return { value: op[1], done: false };case 5: _.label++; y = op[1]; op = [0]; continue;case 7: op = _.ops.pop(); _.trys.pop(); continue;default:if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }if (t[2]) _.ops.pop();_.trys.pop(); continue;}op = body.call(thisArg, _);} catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };}};
-  `;
-
-  let WorkerThread;
-  let workerPromiseFunction;
-
-  // if it's a browser
-  if (checkGlobalVar('window')) {
-    WorkerThread = Worker;
-    workerPromiseFunction = function(functionName, functionDeclaration, args) {
-      const argumentsString = convertArgsToString(args);
-      const execString = `data:text/javascript;charset=US-ASCII,onmessage = function(event) {
-            ${ getTypescriptCompilerHelpers() }
-            ${ functionDeclaration }
-            Promise.resolve()
-                .then(() => ${ functionName }(${ argumentsString }))
-                .then(result => postMessage({ resolve: true, reject: false, data: result }))
-                .catch(err => postMessage({ resolve: false, reject: true, data: err.message }))
+function cleanContext(ctx) {
+  const links = new Map();
+  
+  function recursive(element) {
+    if (Array.isArray(element)) {
+      return element.map((el) => recursive(el));
+    } else if (typeof element === 'object' && element !== null) {
+      const result = {};
+      for (const key in element) {
+        if (Array.isArray(element[key])) {
+          result[key] = recursive(element[key]);
+        } else if (typeof element[key] === 'object' && element[key] !== null) {
+          if (!links.get(element[key])) {
+            links.set(element[key], true);
+            result[key] = recursive(element[key]);
+          } else {
+            result[key] = '[Circular]';
           }
-      `;
-      return new Promise((resolve, reject) => {
-        const worker = new WorkerThread(execString);
-        worker.postMessage(null);
-        worker.onmessage = function(event) {
-          if (event.data.resolve) {
-            return resolve(event.data.data);
-          }
-          if (event.data.reject) {
-            return reject(new Error(event.data.data));
-          }
-        };
-        worker.onerror = function(event) {
-          return reject(event.data);
-        };
-      });
-    };
-    // if it's a Node.js
-  } else if (checkGlobalVar('process')) {
-    const workerThread = require('worker_threads');
-    WorkerThread = workerThread.Worker;
-    workerPromiseFunction = function(functionName, functionDeclaration, args) {
-      const argumentsString = convertArgsToString(args);
-      const execString = `
-        const { workerData, parentPort } = require('worker_threads');
-        ${ getTypescriptCompilerHelpers() }
-        ${ functionDeclaration }
-
-        Promise.resolve()
-          .then(() => ${ functionName }(${ argumentsString }))
-          .then(result => parentPort.postMessage({ resolve: true, reject: false, data: result }))
-          .catch(err => parentPort.postMessage({ resolve: false, reject: true, data: err.stack }))
-      `;
-      return new Promise((resolve, reject) => {
-        let worker;
-        try {
-          worker = new WorkerThread(execString, { eval: true });
-        } catch (err) {
-          reject(err);
+        } else if (typeof element[key] !== 'function') {
+          result[key] = element[key];
         }
+      }
+      return result;
+    }
+  }
+  
+  return recursive(ctx);
+}
+
+function workerCode() {
+  const {workerData, parentPort} = require('worker_threads');
+  
+  // for typescript compilation
+  const __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+      function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+      function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+      function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+      step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+  };
+  
+  void async function () {
+    process.on('uncaughtException', (err) => {
+      console.error('Error in worker thread:', err);
+      process.exit(1);
+    });
+    process.on('unhandledRejection', (err) => {
+      console.error('Error in worker thread:', err);
+      process.exit(1);
+    });
+    
+    const fnInstance = new Function(...workerData.fnArgs.split(','), workerData.fnBody).bind(workerData.context);
+    
+    try {
+      const convertedArgs = workerData.args.map((arg, i) => {
+        if (workerData.functionArgsIndexes.includes(i)) {
+          return new Function(
+            ...arg.substring(arg.indexOf("(") + 1, arg.indexOf(")")).split(','),
+            arg.substring(arg.indexOf("{") + 1, arg.lastIndexOf("}"))).bind(workerData.context);
+        }
+        return arg;
+      });
+      
+      const result = await fnInstance(...convertedArgs);
+      
+      parentPort.postMessage({
+        result: {
+          isResolved: true,
+          result,
+          context: workerData.context,
+        }
+      });
+    } catch (err) {
+      parentPort.postMessage({
+        result: {
+          isResolved: false,
+          result: err.message,
+          context: workerData.context,
+        }
+      });
+    }
+  }();
+}
+
+void function () {
+  Function.prototype.thread = (fn: (...args: any[]) => any, includeRequiredModules: boolean = true) => {
+    if (typeof fn !== 'function') {
+      throw new Error('The first argument must be a function!');
+    }
+    
+    let requiredModulesString = '';
+    let getRequiredModulesStringPromise;
+    if (includeRequiredModules) {
+      getRequiredModulesStringPromise = getRequiredModulesString(getPathToParentModule(), includeRequiredModules);
+    }
+    
+    return async function (...funcArgs) {
+      const {Worker} = require('worker_threads');
+      if (includeRequiredModules) {
+        requiredModulesString += await getRequiredModulesStringPromise;
+      }
+      
+      const functionArgsIndexes = [];
+      const convertedArgs = funcArgs.map((arg, i) => {
+        if (typeof arg === 'function') {
+          functionArgsIndexes.push(i);
+          return arg.toString();
+        }
+        return arg;
+      });
+      
+      const context = typeof this === 'object' && this !== null ? this : {};
+      const worker = new Worker(requiredModulesString + ';' + stringifyFunctionBody(workerCode), {
+        eval: true,
+        workerData: {
+          fnBody: stringifyFunctionBody(fn),
+          fnArgs: stringifyFunctionArguments(fn),
+          context: cleanContext(context),
+          args: convertedArgs,
+          functionArgsIndexes,
+        },
+      });
+      
+      return new Promise((resolve, reject) => {
         worker.on('message', (message) => {
-          if (message.resolve) {
-            return resolve(message.data);
-          }
-          if (message.reject) {
-            return reject(new Error(message.data));
+          if (message.result) {
+            Object.assign(context, message.result.context);
+            if (message.result.isResolved) {
+              resolve(message.result.result);
+            } else {
+              reject(new Error(message.result.result));
+            }
           }
         });
-        worker.on('error', reject);
+        worker.on('error', (err) => reject(err));
         worker.on('exit', (code) => {
           if (code !== 0)
-            reject(new Error(`Worker stopped with exit code ${ code }`));
+            reject(`Worker stopped with exit code ${code}`);
         });
       });
-    };
-  }
-
-  Function.prototype.$ = function(...args) {
-    let functionString = delete this.toString && this.toString();
-    let functionName = this.name || 'function_string_for_execution';
-    let isAsync = this.constructor.name === 'AsyncFunction';
-
-    // Normalize function string
-    // if it is an arrow function
-    if (functionString.search(/^(.+?|)\((.+?|)\)(\s+?|)=>/) >= 0) {
-      functionString = functionString.replace(/^(|.+?)\(/, '(');
-      // if it is a classic function
-    } else if (functionString.search(/^(.+?|)\((.+?|)\)(\s+?|){/) >= 0) {
-      functionString = functionString.replace(/^(|.+?)\(/, 'function(');
     }
-
-    functionString = `const ${ functionName } = ${ isAsync ? 'async' : '' } ${ functionString };`;
-
-    return workerPromiseFunction(functionName, functionString, args);
   };
 }();
